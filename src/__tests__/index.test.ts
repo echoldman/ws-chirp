c107a9c56eac1e5888be28313aba69c11b40860b
db <db@office>
2022-02-08 18:52:33 +0800

    Add ArrayBuffer test.

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,4 +1,5 @@
 import { Chirp } from '../index';
+const md5 = require('md5');
 
 interface Object1 {
   code: number;
@@ -7,30 +8,43 @@ interface Object1 {
 }
 
 test('chirp', () => {
+  const chirp = new Chirp('hello');
+  chirp.addParam('message', 'hello, world!');
+  chirp.addParam('str1', '2020 北京冬奥会');
+  chirp.addParam('str2', '123&&');
+  chirp.addParam('main', 3);
+  chirp.addParam('min', 23.0123);
+  chirp.addParam('count', 9007199254740991);
+  chirp.addParam('age', -9007199254740991)
+
   const object1: Object1 = {
     code: 200,
     values: [1, 2, 3, 4, 5],
     chars: ['a', 'b', 'c']
   }
+  chirp.addParam('object1', object1);
 
   const array1 = ['abc', 10, null];
-
-  const chirp = new Chirp('hello');
-  chirp.addParam('message', 'hello, world!');
-  chirp.addParam('str1', '2020 北京冬奥会');
-  chirp.addParam('str2', '123&&');
-  chirp.addParam('object1', object1);
-  chirp.addParam('main', 3);
-  chirp.addParam('min', 23.0123);
-  chirp.addParam('count', 9007199254740991);
-  chirp.addParam('age', -9007199254740991)
   chirp.addParam('list', array1);
 
-  const buffer = chirp.toArrayBuffer();
-  const new_chirp = Chirp.fromArrayBuffer(buffer);
+  // TODO: 测试大内存复制的性能
+  const a_buffer = new ArrayBuffer(1024*1024*32);
+  const a_view = new DataView(a_buffer);
+  for (let i = 0; i < a_buffer.byteLength; i++) {
+    a_view.setUint8(i, Math.ceil(Math.random() * 255));
+  }
+  chirp.addParam('data', a_buffer);
+  const data_md5 = md5(a_buffer);
+
+  const new_buffer = chirp.toArrayBuffer();
+  const new_chirp = Chirp.fromArrayBuffer(new_buffer);
   expect(new_chirp.getParam('message')).toBe('hello, world!');
   expect(new_chirp.getParam('str1')).toBe('2020 北京冬奥会')
   expect(new_chirp.getParam('str2')).toBe('123&&');
+  expect(new_chirp.getParam('main')).toBe(3);
+  expect(new_chirp.getParam('min')).toBe(23.0123);
+  expect(new_chirp.getParam('count')).toBe(9007199254740991);
+  expect(new_chirp.getParam('age')).toBe(-9007199254740991);
 
   const expect_object1 = chirp.getParam('object1') as Object1;
   expect(expect_object1.code).toBe(200);
@@ -44,13 +58,11 @@ test('chirp', () => {
   expect(expect_object1.chars[1]).toBe('b');
   expect(expect_object1.chars[2]).toBe('c');
 
-  expect(new_chirp.getParam('main')).toBe(3);
-  expect(new_chirp.getParam('min')).toBe(23.0123);
-  expect(new_chirp.getParam('count')).toBe(9007199254740991);
-  expect(new_chirp.getParam('age')).toBe(-9007199254740991);
-
   const expect_array1: Array<any> = new_chirp.getParam('list') as Array<any>;
   expect(expect_array1[0]).toBe('abc');
   expect(expect_array1[1]).toBe(10);
   expect(expect_array1[2]).toBe(null);
+
+  const expect_data = new_chirp.getParam('data') as ArrayBuffer;
+  expect(md5(expect_data)).toBe(data_md5);
 });
